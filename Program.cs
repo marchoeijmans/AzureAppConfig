5214de0de8df16c566bf5b8488ebadfd424e7ed8
marc.hoeijmans <marc.hoeijmans@wolterskluwer.com>
2019-06-22 18:41:23 +0200

    Demo
    Demo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System;
+
+namespace TestAppConfig
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+
+                    config.AddAzureAppConfiguration(options =>
+                        //options.ConnectWithManagedIdentity(settings["AppConfig:Endpoint"]
+                        options.ConnectWithManagedIdentity("https://exampleconfiguration.azconfig.io")
+                            .Use("TestApp:Settings:*")
+                            .WatchAndReloadAll("TestApp:Settings:Sentinal", TimeSpan.FromSeconds(5)));
+                })
+                .UseStartup<Startup>();
+
+
+    }
+}
